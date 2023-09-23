3d70324b9d09480d551ee077773442d1ff706380
lisizu <lisizu@users.noreply.github.com>
2023-09-23 21:17:15 +0800

    Add project files.

---
+++ App1/ConsoleApp1/Boss.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Boss : Enemy
+    {
+        public override void Attack()
+        {
+            //throw new NotImplementedException();
+            Console.WriteLine("aaa");
+        }
+    }
+}
---
+++ App1/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Enemy e = new Boss();
+            e.Attack();
+        }
+
+    }
+
+}
